5700fad898927d4b556dd1474ee990c77861c2f6
unknown <nunezgeorge05@gmail.com>
2024-07-11 17:24:43 +0800

    removed readme.md

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-// postcss.config.js
-module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      // Add other PostCSS plugins here as needed
-    ],
-  };
-  
+import autoprefixer from "autoprefixer";
+import tailwindConfig, { plugins } from "./tailwind.config";
+
+export default{
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
